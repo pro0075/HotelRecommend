6f1023a5d48afe3d21fddd039315d3cf7e0815aa
pratik <pratikpro0076@gmail.com>
2019-02-22 15:51:39 +0530

    first commit

---
+++ HotelRecomend/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelRecomend.Startup))]
+namespace HotelRecomend
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
