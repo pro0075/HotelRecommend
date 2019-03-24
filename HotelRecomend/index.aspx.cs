b6a0a749c3f968c93a2419814fa0feb91502f3f8
pratik <pratikpro0076@gmail.com>
2019-03-24 23:32:03 +0530

    btnsearch

---
+++ HotelRecomend/index.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using HotelRecomandBAL;
+using HotelRecomend;
+using HotelRecomendDAL;
+using HotelRecomendENTITY;
+
+namespace HotelRecomend
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindCity();
+             
+        }
+
+        private void BindCity()
+        {
+            DataSet dsCity = clsHotelRecomendDAL.GetCityList();
+            ddlcity.DataSource = dsCity;
+            ddlcity.DataValueField = "CityId";
+            ddlcity.DataTextField = "CityName";
+            ddlcity.DataBind();
+        }
+
+        protected void ddlcity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HotelRecomendDAL/DBHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Web;
+using System.Configuration;
+
+
+
+/// <summary>
+/// Summary description for DBHelper
+/// </summary>
+public class DBHelper
+{
+    public DBHelper()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    /// <summary>
+    /// extracts the connection string from web config
+    /// </summary>
+    /// <returns>Connection string</returns>
+    public string GetConnectionString()
+    {
+        return ConfigurationManager.ConnectionStrings["Conn"].ToString();
+
+
+    }
+
+    public static string ConnectionString()
+    {
+        return ConfigurationManager.ConnectionStrings["Conn"].ToString();
+
+    }
+}
